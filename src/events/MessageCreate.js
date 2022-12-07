470a5daafa88dec4409af4dbf603401b70969dcf
ReallPanda <thatonepandaguy@yahoo.com>
2022-12-06 20:27:10 -0800

    if user isnt in table it doesnt crash anymore

--- src/events/MessageCreate.js
+++ src/events/MessageCreate.js
@@ -2,35 +2,88 @@ import { Events } from 'discord.js';
 import ms from 'ms';
 import { supabase, cache } from '../index.js';
 
+const Level = async (interaction) => {
+  const interactionAuthorID = interaction.author.id;
+  const interactionCreatedTimestamp = interaction.createdTimestamp;
+  const getData = async (id) => {
+    const { data, error } = await supabase
+      .from('test')
+      .select('uuid::text, createdTimestamp, xp, level')
+      .eq('uuid', interactionAuthorID.toString())
+      .filter('uuid', 'eq', id.toString());
+
+    return { data, error };
+  };
+
+  // create cache if not in cache already
+  if (cache.get(interactionAuthorID) == undefined) {
+    // TODO@FroggyPanda
+    // fix what ever this shit is ;-; and have @ActuallyTomas help?
+    console.log('created cache');
+
+    let db = await getData(interactionAuthorID);
+    console.log(db);
+
+    if (db.data.length === 0) {
+      console.log('insert data');
+      const { error } = await supabase
+        .from('test')
+        .insert({ uuid: interactionAuthorID });
+
+      if (error) console.error(error);
+    }
+
+    db = await getData(interactionAuthorID);
+    console.log(db.data[0]);
+    cache.set(db.data[0].uuid, db.data[0]);
+    console.log('  ', cache.get(interactionAuthorID));
+  }
+
+  // check if msg is older than designated time than one in cache
+  if (
+    interactionCreatedTimestamp >=
+    cache.get(interactionAuthorID).createdTimestamp + ms('5s')
+  ) {
+    console.log('msg is older than 1min than the last msg');
+
+    const GetXP = async () => {
+      const { data, error } = await supabase
+        .from('test')
+        .select('xp')
+        .eq('uuid', interactionAuthorID.toString());
+      if (error) {
+        console.error(error);
+        return;
+      }
+      return data[0].xp;
+    };
+
+    const GetLevel = async () => {
+      const { data, error } = await supabase
+        .from('test')
+        .select('level')
+        .eq('uuid', interactionAuthorID.toString());
+      if (error) {
+        console.error(error);
+        return;
+      }
+      return data[0].level;
+    };
+
+    const newCache = {
+      createdTimestamp: interactionCreatedTimestamp,
+      xp: cache.get(interactionAuthorID).xp + 1 || (await GetXP()) + 1,
+      level: cache.get(interactionAuthorID).level || (await GetLevel()),
+    };
+
+    cache.set(interactionAuthorID, newCache);
+    console.log('  ', cache.get(interactionAuthorID));
+  }
+};
+
 export const event = {
   name: Events.MessageCreate,
   async execute(interaction) {
-    const interactionAuthorID = interaction.author.id;
-    const interactionCreatedTimestamp = interaction.createdTimestamp;
-
-    const { data, error } = await supabase
-      .from('test')
-      .select('uuid::text, createdTimestamp')
-      .eq('uuid', interaction.author.id.toString());
-
-    if (error) {
-      console.log(JSON.stringify.error);
-      return;
-    }
-
-    if (cache.get(interactionAuthorID) == undefined) {
-      cache.set(data[0].uuid, data[0].createdTimestamp);
-      console.log(cache.data);
-      console.log(interaction.createdTimestamp);
-    }
-
-    if (
-      interactionCreatedTimestamp >=
-      cache.get(interactionAuthorID) + ms('1m')
-    ) {
-      console.log('msg is older than 1min than the last msg');
-      cache.set(interactionAuthorID, interactionCreatedTimestamp);
-      console.log(cache.data);
-    }
+    Level(interaction);
   },
 };
